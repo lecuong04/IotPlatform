82ae848b9c8a3922f47fa05c859f7decf7c67294
Le Ngoc Cuong <lengoccuong02042004@gmail.com>
2025-12-12 00:52:38 +0700

    Refactor code structure for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import Dashboard from "./pages/DashBoard_v3";
+import Dashboard from "./pages/DashBoard";
 
 function App() {
   return <Dashboard />;
